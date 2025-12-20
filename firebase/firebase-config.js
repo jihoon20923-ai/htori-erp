acf261673d719d1abca621f8cd7ade34ced6adc1
jihoon20923-ai <jihoon20923@gmail.com>
2025-12-20 11:04:29 +0700

    Update firebase-config.js

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -2,13 +2,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDa….",
-  authDomain: "htori-erp.firebaseapp.com",
-  projectId: "htori-erp",
-  storageBucket: "htori-erp.appspot.com",
-  messagingSenderId: "2917829146",
-  appId: "1:2917829146:web:28129de92f"
+  apiKey: "AIzaSyARxh0wfAo2RMz_-3KsWUUQYfND_F3_3fI",
+  authDomain: "htori-erp-38ed1.firebaseapp.com",
+  databaseURL: "https://htori-erp-38ed1-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "htori-erp-38ed1",
+  storageBucket: "htori-erp-38ed1.firebasestorage.app",
+  messagingSenderId: "653618187246",
+  appId: "1:653618187246:web:cfdf888e1bbb3106cedcff",
+  measurementId: "G-Z3J1NW0KQE"
 };
 
 export const app = initializeApp(firebaseConfig);
